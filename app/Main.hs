cd7b730a5b710408542775f1238fcfeb8e6ee0fc
Matthew Pagan <mastapegs01@gmail.com>
2022-07-11 22:00:59 -0400

    feat: initial tictactoe sprint (#1)

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import TicTacToe (sampleBoard, serializeBoard)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  putStrLn $ serializeBoard sampleBoard
